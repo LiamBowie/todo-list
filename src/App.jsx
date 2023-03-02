5655454812db03ee0c5ca7f1b854e614d42a99c7
Liam Bowie <liambowie16@gmail.com>
2023-03-02 14:14:57 +0000

    Add simple header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Heading from "./components/Heading"
+
 function App() {
 
   return (
-    <div className="App">App</div>
+    <div className="App container py-16 px-6 min-h-screen mx-auto">
+      <Heading />
+    </div>
   )
 }
 
